99ecd72748838982c96258e6bc34f5c0a7d7b220
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-05-13 02:54:42 +0300

    add class Animal

---
+++ src/main/java/by/aston/bakumenko/thirdTask/Animal.java
@@ -0,0 +1,11 @@
+package by.aston.bakumenko.thirdTask;
+
+public class Animal {
+    public void run(int distance) {
+        System.out.println("Животное пробежало " + distance + " м.");
+    }
+
+    public void swim(int distance) {
+        System.out.println("Животное проплыло " + distance + " м.");
+    }
+}
