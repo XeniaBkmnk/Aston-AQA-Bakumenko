87691fc2292c5e63e4333a7eb677a071cf23ae67
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-05-15 13:53:01 +0300

    add class ArrayProcessor

---
+++ src/main/java/by/aston/bakumenko/fourthTask/ArrayProcessor.java
@@ -0,0 +1,23 @@
+package by.aston.bakumenko.fourthTask;
+
+public class ArrayProcessor {
+    public int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Размер массива должен быть 4x4");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    int value = Integer.parseInt(array[i][j]);
+                    sum += value;
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+
+        return sum;
+    }
+}
