de7844b5f989191feb1b74c94a921021fe158b82
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-05-20 23:30:03 +0300

    refactor class ArrayProcessor

--- src/main/java/by/aston/bakumenko/fourthTask/ArrayProcessor.java
+++ src/main/java/by/aston/bakumenko/fourthTask/ArrayProcessor.java
@@ -1,23 +1,36 @@
 package by.aston.bakumenko.fourthTask;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayProcessor {
     public int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
-        if (array.length != 4 || array[0].length != 4) {
+        int columns = array[0].length;
+        int rows = array.length;
+        if (rows != 4 || columns != 4) {
             throw new MyArraySizeException("Размер массива должен быть 4x4");
         }
-
+        for (int i = 0; i < rows; i++) {
+            if (array[i].length != columns) {
+                throw new MyArraySizeException("Размеры строк массива должны быть одинаковыми");
+            }
+        }
         int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
+        List<String> errorMessages = new ArrayList<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 try {
                     int value = Integer.parseInt(array[i][j]);
                     sum += value;
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                    errorMessages.add("Неверные данные в ячейке [" + i + "][" + j + "]");
                 }
             }
+            if (!errorMessages.isEmpty()) {
+                throw new MyArrayDataException(String.join(", ", errorMessages));
+            }
         }
-
         return sum;
     }
 }
