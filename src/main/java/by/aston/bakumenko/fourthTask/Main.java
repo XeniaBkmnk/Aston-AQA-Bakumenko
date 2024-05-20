3f5290470a0f74869e3cdf09b0f46bd9931938d7
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-05-20 23:30:12 +0300

    refactor class Main

--- src/main/java/by/aston/bakumenko/fourthTask/Main.java
+++ src/main/java/by/aston/bakumenko/fourthTask/Main.java
@@ -4,17 +4,18 @@ public class Main {
     public static void main(String[] args) {
         String[][] array = {
                 {"1", "2", "3", "4"},
-                {"5", "6", "7", "8",},
+                {"5", "6", "7", "8"},
                 {"9", "10", "11", "12"},
                 {"13", "14", "15", "16"}
         };
-
         try {
             ArrayProcessor processor = new ArrayProcessor();
             int sum = processor.processArray(array);
             System.out.println("Сумма элементов массива: " + sum);
-        } catch (MyArraySizeException | MyArrayDataException e) {
-            System.out.println("Ошибка: " + e.getMessage());
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера масива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных массива: " + e.getMessage());
         }
     }
 }
