f722535d3bd1badc2a8458f4d95012e0e64d4bf1
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-06-10 18:37:29 +0300

    add class Main

---
+++ src/main/java/by/aston/bakumenko/Main.java
@@ -0,0 +1,23 @@
+package by.aston.bakumenko;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int number = 0;
+            long fact = Factorial.factorial(number);
+            System.out.println("Факториал числа " + number + " равен " + fact);
+            number = 5;
+            fact = Factorial.factorial(number);
+            System.out.println("Факториал числа " + number + " равен " + fact);
+            number = 11;
+            fact = Factorial.factorial(number);
+            System.out.println("Факториал числа " + number + " равен " + fact);
+            number = -1;
+            fact = Factorial.factorial(number);
+            System.out.println("Факториал числа " + number + " равен " + fact);
+        } catch (
+                IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
