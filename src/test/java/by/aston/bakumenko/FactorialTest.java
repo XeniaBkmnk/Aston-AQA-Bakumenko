697beff5979662737705bec3fcc4337e3207e310
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-06-10 20:10:42 +0300

    refactor

--- src/test/java/by/aston/bakumenko/FactorialTest.java
+++ src/test/java/by/aston/bakumenko/FactorialTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class FactorialTest {
+
     @Test(description = "Факториал 0 должен быть 1")
     public void testFactorialOfZero() {
         assertEquals(1, Factorial.factorial(0));
@@ -20,7 +21,8 @@ public class FactorialTest {
         assertEquals(39916800, Factorial.factorial(11));
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
+    @Test(description = "Должно быть выброшено исключение IllegalArgumentException для отрицательного числа",
+            expectedExceptions = IllegalArgumentException.class)
     public void testNegativeNumber() {
         Factorial.factorial(-1);
     }
