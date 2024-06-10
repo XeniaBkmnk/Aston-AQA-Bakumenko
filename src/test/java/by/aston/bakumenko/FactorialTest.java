c2f4e2fa2017071f5a522b74f47b64bd897e6eb6
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-06-10 19:02:24 +0300

    add description

--- src/test/java/by/aston/bakumenko/FactorialTest.java
+++ src/test/java/by/aston/bakumenko/FactorialTest.java
@@ -5,17 +5,17 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class FactorialTest {
-    @Test
+    @Test(description = "Факториал 0 должен быть 1")
     public void testFactorialOfZero() {
         assertEquals(1, Factorial.factorial(0));
     }
 
-    @Test
+    @Test(description = "Факториал 5 должен быть 120")
     public void testFactorialOfFive() {
         assertEquals(120, Factorial.factorial(5));
     }
 
-    @Test
+    @Test(description = "Факториал 11 должен быть 39916800")
     public void testFactorialOfEleven() {
         assertEquals(39916800, Factorial.factorial(11));
     }
