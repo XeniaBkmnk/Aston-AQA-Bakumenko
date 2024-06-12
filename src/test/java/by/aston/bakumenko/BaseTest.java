0844f504803e48fdec80328ebb1ccdf081535ea1
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-06-12 23:56:56 +0300

    add BaseTest

---
+++ src/test/java/by/aston/bakumenko/BaseTest.java
@@ -0,0 +1,20 @@
+package by.aston.bakumenko;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest {
+    WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = WebDriverSingleton.getDriver();
+    }
+
+    @AfterEach
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(4000);
+        WebDriverSingleton.quitDriver();
+    }
+}
