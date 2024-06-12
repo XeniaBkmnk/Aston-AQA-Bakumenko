6e5ba8b7ecf3b7158195ce92ba3b4dd819ba268d
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-06-12 23:59:03 +0300

    add WebDriverSingleton

---
+++ src/main/java/by/aston/bakumenko/WebDriverSingleton.java
@@ -0,0 +1,26 @@
+package by.aston.bakumenko;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverSingleton {
+    private static WebDriver driver;
+
+    private WebDriverSingleton() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
