a01a91850d9936ed3a1cb9423565bd9ddc60af04
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-06-23 19:22:33 +0300

    add @Description

--- src/test/java/by/aston/bakumenko/BaseTest.java
+++ src/test/java/by/aston/bakumenko/BaseTest.java
@@ -1,6 +1,7 @@
 package by.aston.bakumenko;
 
 import by.aston.bakumenko.driver.WebDriverSingleton;
+import jdk.jfr.Description;
 import org.junit.After;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
@@ -9,11 +10,13 @@ public class BaseTest {
     WebDriver driver;
 
     @BeforeEach
+    @Description("Настройка перед выполнением каждого теста")
     public void setUp() {
         driver = WebDriverSingleton.getDriver();
     }
 
     @After
+    @Description("Завершение после выполнения каждого теста")
     public void tearDown() {
         if (WebDriverSingleton.getDriver() != null) {
             WebDriverSingleton.quitDriver();
