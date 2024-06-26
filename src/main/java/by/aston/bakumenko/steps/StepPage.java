786126b37e652d3fd5d37adbe4ebe78df0517b71
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2024-06-27 01:46:26 +0300

    refactor class StepPage

--- src/main/java/by/aston/bakumenko/steps/StepPage.java
+++ src/main/java/by/aston/bakumenko/steps/StepPage.java
@@ -2,19 +2,15 @@ package by.aston.bakumenko.steps;
 
 import by.aston.bakumenko.pages.MtsPage;
 import by.aston.bakumenko.driver.WebDriverSingleton;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 public class StepPage {
     WebDriver driver;
     MtsPage mtsPage;
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public StepPage() {
         this.driver = WebDriverSingleton.getDriver();
         this.mtsPage = new MtsPage();
-        LOGGER.info("WebDriver is created in LoginStep constructor");
     }
 
     public void openMainByMts() {
